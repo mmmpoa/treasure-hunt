f9fcc4ca5f80d14783d546d3a73148c9bb4eccbb
bluscrn <166459891+mmmpoa@users.noreply.github.com>
2025-02-24 19:46:44 -0500

    Create updateos.sh

---
+++ updateos.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Remove ???/goldjewel
+
+cd
+rm -r ???
+
+#Remove Contract (currentyear)/rubynecklace
+
+currentyear=$(Date +%Y)
+cd
+cd Documents
+rm -r Contract $currentyear
+
+#Remove ParisTrip2024/mapoftheworld
+
+cd
+cd Pictures
+rm -r ParisTrip2024
+
+#Remove MathVideos/raspberrypi
+
+cd
+cd Videos
+rm -r MathVideos
+
+#Remove Books/pearls
+
+cd
+cd Bookshelf
+rm -r Books
+
+#Remove minecraftinstaller.zip/crown
+
+cd
+cd Downloads
+rm -r minecraftinstaller.zip
+
+#Remove userkeys/key
+
+cd
+rm -r userkeys
+
+#Remove game1/ending
+
+cd
+cd python_games
+rm -r game1
