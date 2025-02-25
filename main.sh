730f500442384f1e32b687fa0c46b3a53d4f468e
bluscrn <166459891+mmmpoa@users.noreply.github.com>
2025-02-24 19:34:16 -0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+cd
+mkdir ???
+
+#Gold Jewel
+
+cd ???
+touch goldjewel1.txt
+printf "/gylwpv_gnnek" >> goldjewel1.txt
+
+#Ruby Necklace
+
+currentyear=$(Date +%Y)
+cd
+cd Documents
+mkdir Contract $currentyear
+cd Contract
+touch rubynecklace2.txt
+printf "/nrmw1" >> rubynecklace2.txt
+
+#Map of the World
+
+cd
+cd Pictures
+mkdir ParisTrip2024
+cd ParisTrip2024
+touch mapoftheworld3.txt
+printf "hqzigf_fasvs" >> mapoftheworld3.txt
+
+#Raspberry Pi
+
+cd
+cd Videos
+mkdir MathVideos
+cd MathVideos
+touch RaspberryPi4.txt
+printf "/uwbzapuejz" >> RasperryPi4.txt
+
+#Pearls
+
+cd
+cd Bookshelf
+mkdir Books
+cd Books
+touch pearls5.txt
+printf "/glsnfzscsill" >> pearls5.txt
+
+#Crown
+
+cd
+cd Downloads
+mkdir minecraftinstaller.zip
+cd minecraftinstaller.zip
+touch crown6.txt
+printf "/krwptcrr_fnv.aot" >> crown6.txt
+
+#Golden Key
+
+cd
+mkdir userkeys
+cd userkeys
+mkdir key1
+cd key1
+touch golden_and_final_key_vigenere7.txt
+printf "raspbianbash" >> golden_and_final_key7.txt
+
+#Ending
+
+cd
+cd python_games
+mkdir game1
+cd game1
+mkdir sprite_files
+cd sprite_files
+mkdir characters
+cd characters
+mkdir player_sprite
+cd player_sprite
+touch treasure_end.txt
+printf "You did it! Give yourself a pat on the back! \eNow you know simple commands! \eWhen your ready to end the game, type cd, then bash updateos.sh \e(Don't worry this doesn't ACTUALLY update your raspberry pi.)
+
+
+
